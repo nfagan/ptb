7f93381f2980f781edd2972cbfcfde7ad6fc7978
nfagan <nick@fagan.org>
2020-01-23 17:44:37 -0500

    change AnalogOutputPulseManager to more general SingleScanOutputPulseManager

--- +ptb/+signal/SingleScanOutput.m
+++ +ptb/+signal/SingleScanOutput.m
@@ -4,22 +4,69 @@ classdef SingleScanOutput < handle
   end
   
   properties (Access = public)
+    
+    %   DEFAULTOUTPUTVALUES -- Default values to be written each frame.
+    %
+    %     DefaultOutputValues is a 1xN vector of default values for each of
+    %     N output channels. If PersistOutputValues is false, then, each 
+    %     frame, the value of each channel will be reset to its 
+    %     corresponding entry in DefaultOutputValues.
+    %
+    %     See also ptb.signal.SingleScanOutput.PersistOutputValues,
+    %       ptb.signal.SingleScanOutput
     DefaultOuputValues;
+    
+    %   PERSISTOUTPUTVALUES -- True if output values persist once set.
+    %
+    %     PersistOutputValues is a logical scalar indicating whether a
+    %     value written to a channel on a given frame should retain that
+    %     value until explicitly set to a different value. If false, then
+    %     the value of each channel will be reset to its respective
+    %     default at the end of the frame.
+    %
+    %     See also ptb.signal.SingleScanOutput.DefaultOutputValues,
+    %       ptb.signal.SingleScanOutput
+    PersistOutputValues;
   end
   
   properties (SetAccess = private, GetAccess = public)
+    
+    %   PENDINGOUTPUTVALUES -- Values to be written next frame.
+    %
+    %     PendingOutputValues is a 1xN read-only array of values for each 
+    %     of N channels to be written on the next call to `update`.
+    %
+    %     See also ptb.signal.SingleScanOutput.PersistOutputValues,
+    %       ptb.signal.SingleScanOutput
     PendingOutputValues;
+    
+    %   NUMOUTPUTCHANNELS -- Number of output channels in the DAQ session.
+    %
+    %     See also ptb.signal.SingleScanOutput
     NumOutputChannels;
   end
   
   methods
     function obj = SingleScanOutput(session)
+      
+      %   SINGLESCANOUTPUT -- Create SingleScanOutput object instance.
+      %
+      %     obj = ptb.signal.SingleScanOutput( session ); instantiates an
+      %     interface for writing data to all outputs of a DAQ `session` in 
+      %     an immediate-mode / on-demand fashion. That is, values are only
+      %     output upon an explicit call to an `update` function.
+      %
+      %     See also ptb.signal.SingleScanInput.queue_value,
+      %       ptb.signal.SingleScanInput.write_value,
+      %       ptb.signal.SingleScanInput.update
+      
       validateattributes( session, {'daq.Session'}, {'scalar'} ...
         , mfilename, 'session' );
       obj.session = session;
       obj.DefaultOuputValues = make_default_output_values( obj );
       obj.PendingOutputValues = obj.DefaultOuputValues;
       obj.NumOutputChannels = num_channels( obj );
+      obj.PersistOutputValues = false;
     end
     
     function set.DefaultOuputValues(obj, v)
@@ -29,22 +76,75 @@ classdef SingleScanOutput < handle
       obj.DefaultOuputValues = v(:)';
     end
     
+    function set.PersistOutputValues(obj, v)
+      validateattributes( v, {'logical'}, {'scalar'}, mfilename, 'PersistOutputValues' );
+      obj.PersistOutputValues = v;
+    end
+    
     function queue_value(obj, channel, value)
+      
+      %   QUEUE_VALUE -- Set a channel's value to be written this frame.
+      %
+      %     queue_value( obj, channel, value ); sets the entry of 
+      %     PersistOutputValues corresponding to `channel` to `value`, but
+      %     does not write the value to the output.
+      %
+      %     See also ptb.signal.SingleScanOutput,
+      %       ptb.signal.SingleScanOutput.write_value,
+      %       ptb.signal.SingleScanOutput.update
+      
       validate_scalar_channel_and_value( obj, channel, value );
       obj.PendingOutputValues(channel) = value;
     end
     
     function write_value(obj, channel, value)
+      
+      %   WRITE_VALUE -- Queue value and write to the output.
+      %
+      %     write_value( obj, channel, value ); sets the entry of 
+      %     PendingOutputValues corresponding to `channel` to `value`, and
+      %     then immediately calls `update`, such that the value is written
+      %     immediately.
+      %
+      %     See also ptb.signal.SingleScanOutput,
+      %       ptb.signal.SingleScanOutput.queue_value, 
+      %       ptb.signal.SingleScanOutput.update
+      
       queue_value( obj, channel, value );
       update( obj );
     end
     
     function update(obj)
+      
+      %   UPDATE -- Write pending values to the output.
+      %
+      %     update( obj ); writes the values in PendingOutputValues to the
+      %     output. If PersistOutputValues is false, then all 
+      %     PendingOutputValues are immediately reset to their
+      %     DefaultOutputValues. Otherwise, they retain the same value.
+      %
+      %     See also ptb.signal.SingleScanOutput,
+      %       ptb.signal.SingleScanOutput.write_value, 
+      %       ptb.signal.SingleScanOutput.update
+      
       outputSingleScan( obj.session, obj.PendingOutputValues );
-      obj.PendingOutputValues = obj.DefaultOuputValues;
+      
+      if ( ~obj.PersistOutputValues )
+        obj.PendingOutputValues = obj.DefaultOuputValues;
+      end
     end
     
     function validate_scalar_channel_and_value(obj, channel, value)
+      
+      %   VALIDATE_SCALAR_CHANNEL_AND_VALUE -- Ensure valid channel and value.
+      %
+      %     validate_scalar_channel_and_value( obj, channel, value );
+      %     throws an error if `channel` or `value` are non-scalar, or if
+      %     `channel` is out of bounds of the number of output channels in
+      %     the DAQ session.
+      %
+      %     See also ptb.signal.SingleScanOutput
+      
       validate_channel_index( obj, channel );
       
       if ( ~isscalar(value) )
@@ -54,13 +154,15 @@ classdef SingleScanOutput < handle
     
     function validate_channel_index(obj, channel)
       if ( channel > obj.NumOutputChannels || channel < 1 )
-        error( 'Channel %d is out of bounds of the number of channels (%d)' ...
+        error( 'Channel %d is out of bounds of the number of channels (%d).' ...
           , channel, obj.NumOutputChannels );
       end
     end
     
     function delete(obj)
       try
+        %   Reset values to their defaults before updating.
+        obj.PendingOutputValues = obj.DefaultOuputValues;
         update( obj );
       catch err
         warning( err.message );
---
+++ +ptb/+signal/SingleScanOutputPulseManager.m
@@ -0,0 +1,147 @@
+classdef SingleScanOutputPulseManager < handle
+  properties (Access = private)
+    scan_output;
+    channel_index;
+    
+    active_pulse_timer;
+    active_pulse_time;
+    
+    pending_pulse_times;
+  end
+  
+  properties (Access = public)
+    
+    %   LOW -- Low value written upon pulse terminaton.
+    %
+    %     See also ptb.signal.AnalogOutputPulseManager, 
+    %       ptb.signal.AnalogOutputPulseManager.High
+    Low = 0;
+    
+    %   HIGH -- High value written upon pulse onset.
+    %
+    %     See also ptb.signal.AnalogOutputPulseManager, 
+    %       ptb.signal.AnalogOutputPulseManager.Low
+    High = 5;
+  end
+  
+  methods
+    function obj = SingleScanOutputPulseManager(scan_output, channel_index)
+      
+      %   SINGLESCANOUTPUTPULSEMANAGER -- Create pulse manager instance.
+      %
+      %     obj = ptb.signal.SingleScanOutputPulseManager( single_scan_output, channel_index );
+      %     instantiates an interface for writing timed pulses to a DAQ
+      %     session output. `single_scan_output` is a
+      %     ptb.signal.SingleScanOutput object representing the set of
+      %     single scan output channels in the current DAQ session, and
+      %     `channel_index` is the 1-based index into that channel set.
+      %
+      %     See also ptb.signal.trigger, ptb.signal.update
+      
+      validateattributes( scan_output, {'ptb.signal.SingleScanOutput'} ...
+        , {'scalar'}, mfilename, 'scan_output' );
+      
+      validate_channel_index( scan_output, channel_index );
+      
+      obj.scan_output = scan_output;
+      obj.channel_index = channel_index;
+    end
+    
+    function set.Low(obj, v)
+      validate_voltage( obj, v, 'Low' );
+      obj.Low = v;
+    end
+    
+    function set.High(obj, v)
+      validate_voltage( obj, v, 'High' );
+      obj.High = v;
+    end
+    
+    function update(obj) 
+      
+      %   UPDATE -- Update active and pending pulses.
+      %
+      %     update( obj ); considers pending and active pulses. If the 
+      %     active pulse has elapsed, it is terminated by writing Low to 
+      %     the output. If a pending pulse exists, it is made active and 
+      %     removed from the queue.
+      %
+      %     See also ptb.signal.SingleScanOutputPulseManager
+      
+      if ( ~isempty(obj.active_pulse_timer) )
+        if ( elapsed_active(obj) )
+          write_low_clear_active( obj );
+        else
+          queue_write_high( obj );
+        end
+      end
+      
+      if ( ~isempty(obj.active_pulse_timer) || isempty(obj.pending_pulse_times) )
+        return;
+      end
+      
+      write_high_activate( obj, obj.pending_pulse_times(1) );      
+      obj.pending_pulse_times(1) = [];
+    end
+    
+    function trigger(obj, pulse_time)
+      
+      %   TRIGGER -- Trigger pulse, or queue it if an active pulse exists.
+      %
+      %     trigger( obj, pulse_time ), when there are no active or pending
+      %     pulses, immediately writes High to the output, and returns. In
+      %     subsequent calls to `update`, Low will be written to the output
+      %     once `pulse_time` seconds have elapsed.
+      %
+      %     trigger( obj, pulse_time ), when there are active or pending
+      %     pulses, adds the pulse to the queue. In subsequent calls to
+      %     `update`, the pulse will be triggered once all pending pulses
+      %     have been triggered.
+      %
+      %     See also ptb.SingleScanOutputPulseManager,
+      %       ptb.SingleScanOutputPulseManager.update
+      
+      if ( isempty(obj.pending_pulse_times) && elapsed_active(obj) )        
+        write_high_activate( obj, pulse_time );
+      else
+        obj.pending_pulse_times(end+1) = pulse_time;
+      end
+    end
+    
+    function delete(obj)
+      try
+        write_low_clear_active( obj );
+      catch err
+        warning( err.message );
+      end
+    end
+  end
+  
+  methods (Access = private)
+    function validate_voltage(obj, v, name)
+      validateattributes( v, {'double'}, {'scalar', 'nonnan', 'finite'} ...
+        , mfilename, name );
+    end
+    
+    function tf = elapsed_active(obj)
+      tf = isempty( obj.active_pulse_timer ) || ...
+        toc( obj.active_pulse_timer ) >= obj.active_pulse_time;
+    end
+    
+    function queue_write_high(obj)
+      queue_value( obj.scan_output, obj.channel_index, obj.High );
+    end
+    
+    function write_low_clear_active(obj)
+      write_value( obj.scan_output, obj.channel_index, obj.Low );
+      obj.active_pulse_timer = [];
+      obj.active_pulse_time = nan;
+    end
+    
+    function write_high_activate(obj, pulse_time)
+      write_value( obj.scan_output, obj.channel_index, obj.High );
+      obj.active_pulse_timer = tic();
+      obj.active_pulse_time = pulse_time;
+    end
+  end
+end
